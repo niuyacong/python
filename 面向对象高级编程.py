b196e0332a8b197db60d48bebd3f9dc4cbd1d624
niuyacong <1139800084@qq.com>
2018-01-22 17:15:14 +0800

    定制类
    Signed-off-by: niuyacong <1139800084@qq.com>

--- 面向对象高级编程.py
+++ 面向对象高级编程.py
@@ -8,7 +8,7 @@ __author__ = 'nyc'
 # __slots__
 # @property
 # 多重继承
-# 定制类
+# 定制类(__str__、__repr__、__iter__、__getitem__、__getattr__、__call__)
 
 
 # __slots__
@@ -243,8 +243,117 @@ class Student(object):
         self.name=value;
     def __str__(self):
         return 'Student object (name %s )'%self.name
-
     __repr__ = __str__
+#
+#
+# __iter__
+#
+# 如果一个类想被用于for ... in循环，类似list或tuple那样，就必须实现一个__iter__()方法，该方法返回一个迭代对象，然后，
+# Python的for循环就会不断调用该迭代对象的__next__()方法拿到循环的下一个值，直到遇到StopIteration错误时退出循环。
+#
+# 我们以斐波那契数列为例，写一个Fib类，可以作用于for循环：
+class Fib(object):
+    def __init__(self):
+        self.a,self.b=0,1; # 初始化两个计时器
+    def __iter__(self):
+        return  self; # 实例本身就是迭代对象，所以返回自身
+    def __next__(self):
+        self.a,self.b=self.b,self.a+self.b;
+        if self.a>10000:
+            raise StopIteration();
+        return  self.a;
+for n in Fib():
+    print(n);
+#
+#
+# __getitem__
+#
+# Fib实例虽然能作用于for循环，看起来和list有点像，但是，把它当成list来使用还是不行，比如，取第5个元素：
+# print(Fib()[5]); # 异常：TypeError: 'Fib' object does not support indexing
+# 要表现得像list那样按照下标取出元素，需要实现__getitem__()方法：
+class Fib(object):
+    def __getitem__(self, item):
+        a,b=1,1;
+        for x in range(item):
+            a,b=b,a+b;
+        return  a;
+print(Fib()[5]);  # 8
+# amazing : 但是list有个神奇的切片方法：
+print(list(range(100))[5:10]);# [5, 6, 7, 8, 9]
+# 对于Fib却报错。原因是__getitem__()传入的参数可能是一个int，也可能是一个切片对象slice，所以要做判断：
+# print(Fib()[5:10]);  # 异常：TypeError: 'slice' object cannot be interpreted as an integer
+class Fib(object):
+    def __getitem__(self, item):
+        if isinstance(item,int):# 如果传入是int类型
+            a,b=1,1;
+            for x in range(100):
+                a,b=b,a+b;
+            return a;
+        if isinstance(item,slice):
+            start=item.start;
+            stop=item.stop;
+            if start is None:
+                start=0;
+            a,b=1,1;
+            l=[];
+            for x in range(stop):
+                if x>=start:
+                    l.append(a);
+                a,b=b,a+b;
+            return  l;
+print(Fib()[0:5]); # [1, 1, 2, 3, 5]
+# 也没有对负数作处理，所以，要正确实现一个__getitem__()还是有很多工作要做的。
+#
+# 此外，如果把对象看成dict，__getitem__()的参数也可能是一个可以作key的object，例如str。
+#
+# 与之对应的是__setitem__()方法，把对象视作list或dict来对集合赋值。最后，还有一个__delitem__()方法，用于删除某个元素。
+#
+# 总之，通过上面的方法，我们自己定义的类表现得和Python自带的list、tuple、dict没什么区别，这完全归功于动态语言的“鸭子类型”，不需要强制继承某个接口。
+#
+# __getattr__
+# 正常情况下，当我们调用类的方法或属性时，如果不存在，就会报错。比如定义Student类
+# 要避免这个错误，除了可以加上一个score属性外，Python还有另一个机制，那就是写一个__getattr__()方法，动态返回一个属性。修改如下：
+class Student(object):
+     def __init__(self):
+         self.name = 'Michael';
+     def __getattr__(self, item):
+         if item == 'score':
+             return 99;
+a=Student();
+print(a.score); # 99
+# 返回函数也是完全可以的：
+class Student(object):
+    def __getattr__(self, item):
+        if item=="score":
+            return  lambda :25;
+        raise AttributeError('Student object has no attribute \'%s\'' % item)
+a=Student();
+print(a.score());# 25
+# print(a.age()); # AttributeError: Student object has no attribute 'age'
+# __call__
+#
+# 一个对象实例可以有自己的属性和方法，当我们调用实例方法时，我们用instance.method()来调用。能不能直接在实例本身上调用呢？在Python中，答案是肯定的。
+#
+# 任何类，只需要定义一个__call__()方法，就可以直接对实例进行调用。请看示例：
+class Student(object):
+    def __init__(self,name):
+        self.name=name;
+    def __call__(self):
+        print("my name is %s" % self.name);
+s=Student('nyc');
+s();  # my name is nyc
+# 如果你把对象看成函数，那么函数本身其实也可以在运行期动态创建出来，因为类的实例都是运行期创建出来的，
+# 这么一来，我们就模糊了对象和函数的界限。
+#
+# 那么，怎么判断一个变量是对象还是函数呢？其实，更多的时候，我们需要判断一个对象是否能被调用，
+# 能被调用的对象就是一个Callable对象，比如函数和我们上面定义的带有__call__()的类实例：
+print(callable(Student('nyc'))); #True
+print(callable(max)); # True
+print(callable([1,2,3])) # False
+print(callable(None)); # False
+print(callable('str')); # False
+
+
 
 
 
